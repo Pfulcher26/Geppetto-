c4199b7656cbbe8e177018a5d7f22ad1f65f975e
pfulcher26 <pfulcher26@gmail.com>
2022-10-10 16:41:04 -0700

    Code freeze day one.

--- src/components/PuppetEntry/PuppetEntry.jsx
+++ src/components/PuppetEntry/PuppetEntry.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import styles from "./puppet-entry.module.css";
 
 export default function PuppetEntry({item, index}) {
   return (
     <>
     <div key={index}>
-        <p>{item.name}</p>
-        <p>{item.story}</p>
+        <div className={styles.entries}>
+    <br></br><p><strong>{item.name}</strong></p>
+        <p>{item.story}</p><br></br>
+    </div>
     </div>
     </>
   )
