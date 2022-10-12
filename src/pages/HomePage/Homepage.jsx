dd04b3c80f65de332d144f55f2051ed792f05797
pfulcher26 <pfulcher26@gmail.com>
2022-10-12 14:29:13 -0700

    Addsa caveat to the homepage about using AI responsibily and provides a link so that concerned users can send an email.

--- src/pages/HomePage/Homepage.jsx
+++ src/pages/HomePage/Homepage.jsx
@@ -8,6 +8,9 @@ export default function Homepage() {
         <h1>Welcome to Geppetto</h1>
         <span className={styles.description}><p>A website that utilizes the DaVinci 2 AI to let you wish upon a falling star.</p></span>
         <img className={styles.image} src="/images/awe.jpg"/>
+        <p>AI is a powerful tool with boundless potential, as such it is important that we use it responsibly and with humanity.</p>
+        <p>Users with any concerns, please feel free to contact us below.</p>
+        <a href = "mailto: pfulcher26@gmail.com" className={styles.email}>Send Email</a>
     </div>
   )
 }
